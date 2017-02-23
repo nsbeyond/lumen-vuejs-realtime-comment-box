d2ebb561eacff4dcc89c00c34d9022ef62adf11e
SalapaO <nsbeyond@windowslive.com>
2017-02-23 15:45:30 +0700

    remove logs, node_modules folder

--- backoffice/node_modules/vuetable/src/vue-table.js
+++
@@ -1,5 +0,0 @@
-Vue.component('vuetable-pagination', require('./components/VuetablePagination.vue'));
-
-Vue.component('vuetable-pagination-dropdown', require('./components/VuetablePaginationDropdown.vue'));
-
-Vue.component('vuetable', require('./components/Vuetable.vue'));
